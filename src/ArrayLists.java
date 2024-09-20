285badc86ef83e41c7f5706b7fd20bdf76226c08
Ollie1994 <oliwer.carlsson.jobb@gmail.com>
2024-09-20 15:03:34 +0200

    allt funkar utom dubletter

---
+++ src/ArrayLists.java
@@ -0,0 +1,9 @@
+public class ArrayLists {
+Main main = new Main();
+    public Object addToArray(Object add) {
+        return add;
+    }
+    public Object removeFromArray(Object remove) {
+        return remove;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayLists arrayMetod = new ArrayLists();
+        ArrayList<Object> shoppingList = new ArrayList<>();
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Available actions!");
+            System.out.println("0. To shutdown!");
+            System.out.println("1. To add item(s) to list  (comma delimited list)");
+            System.out.println("2. To remove any items (comma delimited list)");
+            System.out.println("Enter the number for which action you want to take");
+//-------------------------------------------------------------------------------------
+            switch (sc.nextInt()) {
+                case 0:
+                    return;
+                case 1:
+                    System.out.println("Type in the shopping item you would like to add");
+                    Object add = sc.next();
+                    arrayMetod.addToArray(add);
+                    shoppingList.add(add);
+                    shoppingList.sort(null);
+                    System.out.println(shoppingList.toString());
+                    break;
+                case 2:
+                    System.out.println("Type in the shopping item you would like to remove");
+                    Object remove = sc.next();
+                    arrayMetod.removeFromArray(remove);
+                    shoppingList.remove(remove);
+                    shoppingList.sort(null);
+                    System.out.println(shoppingList.toString());
+                    break;
+            }
+        }
     }
 }
